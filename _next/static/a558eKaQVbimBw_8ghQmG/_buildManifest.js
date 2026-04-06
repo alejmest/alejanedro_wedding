d1d90539b96fb39741f9217e889dff9b1e261374
Alejandro Mestanza <alejandro.mestanza@gmail.com>
2026-04-06 13:44:32 -0400

    Updates

--- _next/static/a558eKaQVbimBw_8ghQmG/_buildManifest.js
+++ _next/static/a558eKaQVbimBw_8ghQmG/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/alejanedro_wedding//_next/:path+",
+        "destination": "/alejanedro_wedding/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
