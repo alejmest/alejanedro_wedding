b6218c15a4f70dcefa12b8bcc1117b7541a4113e
Alejandro Mestanza <alejandro.mestanza@gmail.com>
2026-04-06 13:41:52 -0400

    update to basePath var in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = 'alejanedro_wedding'
 const nextConfig = {
     output: 'export',
+    basePath: `/${repoName}`,
+    assetPrefix: `/${repoName}`,
 };
 
 module.exports = nextConfig;
